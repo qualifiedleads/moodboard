8bbf4e87e0c1f388e40102f6b1a37e6076da58c7
Thomas Henley <thomas@hendore.co.uk>
2013-07-24 17:34:45 +0100

    Fixed syntax error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify({status: 'Working', nextStep: 'Node Modules'});
+  res.write(JSON.stringify({status: 'Working', nextStep: 'Node Modules'}));
   res.end();
 }).listen(process.env.PORT || 9000);
 
