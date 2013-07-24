1e93f0e07d58669fb9dd83b986097136fbe1667e
Thomas Henley <thomas@hendore.co.uk>
2013-07-24 17:47:15 +0100

    Express framework no routes

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var http = require('http');
+// The application is built ontop of the express framework
+// Docs: http://expressjs.com/api.html
+var express = require('express');
+app = express();
 
-http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify({status: 'Working', nextStep: 'Node Modules'}));
-  res.end();
-}).listen(process.env.PORT || 9000);
+// Use Jade Template engine
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'jade');
 
+// Setup common middleware
+app.use(express.logger());
+app.use(express.compress());
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
+
+// Setup some application level variables
+app.set('appName', 'Picture Board');
+app.set('emails', {
+    support: 'support@conect.ac'
+});
+
+// Setup application routes
+require('./routes');
+
+// Start listening for incoming requests
+app.listen(process.env.PORT || 5000);
